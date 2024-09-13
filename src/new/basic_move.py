4490a142dbc86340f1e86603e0adace6351dd70a
bugxch <bugxch@gmail.com>
2024-09-13 21:25:10 +0800

    add new proc

---
+++ src/new/basic_move.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+* @par Copyright (C): 2024-2024, hunan CLB Tech
+* @file         Basic_movement
+* @version      V2.0
+* @details
+* @par History
+
+@author: bugxch
+"""
+import RPi.GPIO as GPIO
+import time
+from gpiozero import Button, LED
+
+
+PWMA = 18
+AIN1 = 22
+AIN2 = 27
+
+PWMB = 23
+BIN1 = 25
+BIN2 = 24
+
+
+def t_up(speed, t_time):
+    L_Motor.ChangeDutyCycle(speed)
+    GPIO.output(AIN2, False)  # AIN2
+    GPIO.output(AIN1, True)  # AIN1
+
+    R_Motor.ChangeDutyCycle(speed)
+    GPIO.output(BIN2, False)  # BIN2
+    GPIO.output(BIN1, True)  # BIN1
+    time.sleep(t_time)
+
+
+def t_stop(t_time):
+    L_Motor.ChangeDutyCycle(0)
+    GPIO.output(AIN2, False)  # AIN2
+    GPIO.output(AIN1, False)  # AIN1
+
+    R_Motor.ChangeDutyCycle(0)
+    GPIO.output(BIN2, False)  # BIN2
+    GPIO.output(BIN1, False)  # BIN1
+    time.sleep(t_time)
+
+
+def t_down(speed, t_time):
+    L_Motor.ChangeDutyCycle(speed)
+    GPIO.output(AIN2, True)  # AIN2
+    GPIO.output(AIN1, False)  # AIN1
+
+    R_Motor.ChangeDutyCycle(speed)
+    GPIO.output(BIN2, True)  # BIN2
+    GPIO.output(BIN1, False)  # BIN1
+    time.sleep(t_time)
+
+
+def t_left(speed, t_time):
+    L_Motor.ChangeDutyCycle(speed)
+    GPIO.output(AIN2, True)  # AIN2
+    GPIO.output(AIN1, False)  # AIN1
+
+    R_Motor.ChangeDutyCycle(speed)
+    GPIO.output(BIN2, False)  # BIN2
+    GPIO.output(BIN1, True)  # BIN1
+    time.sleep(t_time)
+
+
+def t_right(speed, t_time):
+    L_Motor.ChangeDutyCycle(speed)
+    GPIO.output(AIN2, False)  # AIN2
+    GPIO.output(AIN1, True)  # AIN1
+
+    R_Motor.ChangeDutyCycle(speed)
+    GPIO.output(BIN2, True)  # BIN2
+    GPIO.output(BIN1, False)  # BIN1
+    time.sleep(t_time)
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(AIN2, GPIO.OUT)
+GPIO.setup(AIN1, GPIO.OUT)
+GPIO.setup(PWMA, GPIO.OUT)
+
+GPIO.setup(BIN1, GPIO.OUT)
+GPIO.setup(BIN2, GPIO.OUT)
+GPIO.setup(PWMB, GPIO.OUT)
+
+L_Motor = GPIO.PWM(PWMA, 100)
+L_Motor.start(0)
+
+R_Motor = GPIO.PWM(PWMB, 100)
+R_Motor.start(0)
+
+try:
+    while True:
+        # t_up(50, 3)
+        # t_down(50, 3)
+        t_left(30, 3)
+        t_right(100, 3)
+        # t_stop(3)
+except KeyboardInterrupt:
+    GPIO.cleanup()
